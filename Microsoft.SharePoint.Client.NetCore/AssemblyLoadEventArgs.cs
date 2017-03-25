833fe453cd502ce50c8d41ec6418a08717d42b67
Radi A <radi.a@live.com.au>
2017-03-25 19:38:36 +0200

    Property Values and Assembly loading args

--- Microsoft.SharePoint.Client.NetCore/AssemblyLoadEventArgs.cs
+++ Microsoft.SharePoint.Client.NetCore/AssemblyLoadEventArgs.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Microsoft.SharePoint.Client.NetCore.Runtime
 {
-    //This is temporary for building
-    //needs to be removed
     internal class AssemblyLoadEventArgs : EventArgs
     {
+        private Assembly _LoadedAssembly;
+
+        /// <summary>Gets an <see cref="T:System.Reflection.Assembly" /> that represents the currently loaded assembly.</summary>
+        /// <returns>An instance of <see cref="T:System.Reflection.Assembly" /> that represents the currently loaded assembly.</returns>
+        /// <filterpriority>2</filterpriority>
+        public Assembly LoadedAssembly
+        {
+            get
+            {
+                return this._LoadedAssembly;
+            }
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.AssemblyLoadEventArgs" /> class using the specified <see cref="T:System.Reflection.Assembly" />.</summary>
+        /// <param name="loadedAssembly">An instance that represents the currently loaded assembly. </param>
+        public AssemblyLoadEventArgs(Assembly loadedAssembly)
+        {
+            this._LoadedAssembly = loadedAssembly;
+        }
     }
 }
---
+++ Microsoft.SharePoint.Client.NetCore/PropertyValues.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.PropertyValues", ServerTypeId = "{3973524d-2d5a-4683-aa39-38a2acc6e63c}")]
+    public class PropertyValues : ClientObject
+    {
+        public Dictionary<string, object> FieldValues
+        {
+            get
+            {
+                Dictionary<string, object> dictionary = null;
+                object obj = null;
+                if (base.ObjectData.MethodReturnObjects.TryGetValue("$m_dict", out obj))
+                {
+                    dictionary = (obj as Dictionary<string, object>);
+                }
+                if (dictionary == null)
+                {
+                    dictionary = new Dictionary<string, object>();
+                    base.ObjectData.MethodReturnObjects["$m_dict"] = dictionary;
+                }
+                return dictionary;
+            }
+        }
+
+        public object this[string fieldName]
+        {
+            [PseudoRemote(Name = "GetFieldValue")]
+            get
+            {
+                return this.GetFieldValue(fieldName);
+            }
+            [Remote(Name = "SetFieldValue")]
+            set
+            {
+                this.SetFieldValue(fieldName, value);
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public PropertyValues(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+
+        protected override void InitNonPropertyFieldFromJson(string peekedName, JsonReader reader)
+        {
+            KeyValuePair<string, object> keyValuePair = reader.ReadKeyValue();
+            this.FieldValues[keyValuePair.Key] = keyValuePair.Value;
+        }
+
+        [PseudoRemote]
+        internal object GetFieldValue(string fieldName)
+        {
+            object result = null;
+            if (this.FieldValues.TryGetValue(fieldName, out result))
+            {
+                return result;
+            }
+            throw new PropertyOrFieldNotInitializedException();
+        }
+
+        public override void RefreshLoad()
+        {
+            base.RefreshLoad();
+            this.LoadExpandoFields();
+        }
+
+        protected override void LoadExpandoFields()
+        {
+            foreach (string current in this.FieldValues.Keys)
+            {
+                base.Retrieve(new string[]
+                {
+                    current
+                });
+            }
+        }
+
+        [Remote]
+        internal virtual void SetFieldValue(string fieldName, object value)
+        {
+            ClientRuntimeContext context = base.Context;
+            this.FieldValues[fieldName] = value;
+            ClientAction query = new ClientActionInvokeMethod(this, "SetFieldValue", new object[]
+            {
+                fieldName,
+                value
+            });
+            if (context != null)
+            {
+                context.AddQuery(query);
+            }
+        }
+    }
+}
