11ee28599270ccdd1f6b47d71a5b0e594f4c25a1
Radi A <radi.a@live.com.au>
2017-03-25 18:38:55 +0200

    Added the ChangeType enum

---
+++ Microsoft.SharePoint.Client.NetCore/ChangeType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    public enum ChangeType
+    {
+        NoChange,
+        Add,
+        Update,
+        DeleteObject,
+        Rename,
+        MoveAway,
+        MoveInto,
+        Restore,
+        RoleAdd,
+        RoleDelete,
+        RoleUpdate,
+        AssignmentAdd,
+        AssignmentDelete,
+        MemberAdd,
+        MemberDelete,
+        SystemUpdate,
+        Navigation,
+        ScopeAdd,
+        ScopeDelete,
+        ListContentTypeAdd,
+        ListContentTypeDelete,
+        Dirty,
+        Activity
+    }
+}
