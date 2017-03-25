d11900e26cce9551e38a424d4a3a8f9c93bf4ff2
Radi A <radi.a@live.com.au>
2017-03-25 18:39:05 +0200

    Goup Property names

---
+++ Microsoft.SharePoint.Client.NetCore/GroupObjectPropertyNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class GroupObjectPropertyNames
+    {
+        public const string Owner = "Owner";
+
+        public const string Users = "Users";
+    }
+}
---
+++ Microsoft.SharePoint.Client.NetCore/GroupPropertyNames.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class GroupPropertyNames
+    {
+        public const string AllowMembersEditMembership = "AllowMembersEditMembership";
+
+        public const string AllowRequestToJoinLeave = "AllowRequestToJoinLeave";
+
+        public const string AutoAcceptRequestToJoinLeave = "AutoAcceptRequestToJoinLeave";
+
+        public const string CanCurrentUserEditMembership = "CanCurrentUserEditMembership";
+
+        public const string CanCurrentUserManageGroup = "CanCurrentUserManageGroup";
+
+        public const string CanCurrentUserViewMembership = "CanCurrentUserViewMembership";
+
+        public const string Description = "Description";
+
+        public const string OnlyAllowMembersViewMembership = "OnlyAllowMembersViewMembership";
+
+        public const string OwnerTitle = "OwnerTitle";
+
+        public const string RequestToJoinLeaveEmailSetting = "RequestToJoinLeaveEmailSetting";
+    }
+}
