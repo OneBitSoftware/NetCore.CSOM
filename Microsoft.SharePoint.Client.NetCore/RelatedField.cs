4c44587f9cf8b62e23ee86969531a7d1f80d29a6
Radi A <radi.a@live.com.au>
2017-03-26 19:25:44 +0300

    Related Fields

---
+++ Microsoft.SharePoint.Client.NetCore/RelatedField.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System;
+using System.ComponentModel;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.RelatedField", ServerTypeId = "{a91deb1a-2f24-4ac5-a1a6-3b1e530c307f}")]
+    public class RelatedField : ClientObject
+    {
+        [Remote]
+        public Guid FieldId
+        {
+            get
+            {
+                base.CheckUninitializedProperty("FieldId");
+                return (Guid)base.ObjectData.Properties["FieldId"];
+            }
+        }
+
+        [Remote]
+        public Guid ListId
+        {
+            get
+            {
+                base.CheckUninitializedProperty("ListId");
+                return (Guid)base.ObjectData.Properties["ListId"];
+            }
+        }
+
+        [Remote]
+        public List LookupList
+        {
+            get
+            {
+                object obj;
+                List list;
+                if (base.ObjectData.ClientObjectProperties.TryGetValue("LookupList", out obj))
+                {
+                    list = (List)obj;
+                }
+                else
+                {
+                    list = new List(base.Context, new ObjectPathProperty(base.Context, base.Path, "LookupList"));
+                    base.ObjectData.ClientObjectProperties["LookupList"] = list;
+                }
+                return list;
+            }
+        }
+
+        [Remote]
+        public RelationshipDeleteBehaviorType RelationshipDeleteBehavior
+        {
+            get
+            {
+                base.CheckUninitializedProperty("RelationshipDeleteBehavior");
+                return (RelationshipDeleteBehaviorType)base.ObjectData.Properties["RelationshipDeleteBehavior"];
+            }
+        }
+
+        [Remote]
+        public Guid WebId
+        {
+            get
+            {
+                base.CheckUninitializedProperty("WebId");
+                return (Guid)base.ObjectData.Properties["WebId"];
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public RelatedField(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+
+        protected override bool InitOnePropertyFromJson(string peekedName, JsonReader reader)
+        {
+            bool flag = base.InitOnePropertyFromJson(peekedName, reader);
+            if (flag)
+            {
+                return flag;
+            }
+            if (peekedName != null)
+            {
+                if (!(peekedName == "FieldId"))
+                {
+                    if (!(peekedName == "ListId"))
+                    {
+                        if (!(peekedName == "LookupList"))
+                        {
+                            if (!(peekedName == "RelationshipDeleteBehavior"))
+                            {
+                                if (peekedName == "WebId")
+                                {
+                                    flag = true;
+                                    reader.ReadName();
+                                    base.ObjectData.Properties["WebId"] = reader.ReadGuid();
+                                }
+                            }
+                            else
+                            {
+                                flag = true;
+                                reader.ReadName();
+                                base.ObjectData.Properties["RelationshipDeleteBehavior"] = reader.ReadEnum<RelationshipDeleteBehaviorType>();
+                            }
+                        }
+                        else
+                        {
+                            flag = true;
+                            reader.ReadName();
+                            base.UpdateClientObjectPropertyType("LookupList", this.LookupList, reader);
+                            this.LookupList.FromJson(reader);
+                        }
+                    }
+                    else
+                    {
+                        flag = true;
+                        reader.ReadName();
+                        base.ObjectData.Properties["ListId"] = reader.ReadGuid();
+                    }
+                }
+                else
+                {
+                    flag = true;
+                    reader.ReadName();
+                    base.ObjectData.Properties["FieldId"] = reader.ReadGuid();
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ Microsoft.SharePoint.Client.NetCore/RelatedFieldCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System.ComponentModel;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.RelatedFieldCollection", ServerTypeId = "{4703193f-e3ed-445b-8612-7c6218e0eb5a}")]
+    public class RelatedFieldCollection : ClientObjectCollection<RelatedField>
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public RelatedFieldCollection(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+    }
+}
