b2403cb914f35c851b8af00b657851318605ee16
Radi A <radi.a@live.com.au>
2017-03-26 16:56:26 +0300

    Fixed build issue with old reference/using

--- Microsoft.SharePoint.Client.NetCore/Runtime/ScriptTypeMap.cs
+++ Microsoft.SharePoint.Client.NetCore/Runtime/ScriptTypeMap.cs
@@ -6,7 +6,6 @@ using System.Xml;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyModel;
 
 namespace Microsoft.SharePoint.Client.NetCore.Runtime
 {
