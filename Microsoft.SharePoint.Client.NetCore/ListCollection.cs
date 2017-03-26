a342d6a3b9f479e43b6302dbf8f797c32ea6c6aa
Radi A <radi.a@live.com.au>
2017-03-26 20:17:39 +0300

    Added List COllection and Tempalte Collection

---
+++ Microsoft.SharePoint.Client.NetCore/ListCollection.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.ListCollection", ServerTypeId = "{1e78b736-61f0-441c-a785-10fc25387c8d}")]
+    public class ListCollection : ClientObjectCollection<List>
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ListCollection(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+
+        [Remote]
+        public List GetByTitle(string title)
+        {
+            ClientRuntimeContext context = base.Context;
+            if (base.Context.ValidateOnClient)
+            {
+                if (title == null)
+                {
+                    throw ClientUtility.CreateArgumentNullException("title");
+                }
+                if (title != null && title.Length == 0)
+                {
+                    throw ClientUtility.CreateArgumentException("title");
+                }
+                if (title != null && title.Length > 255)
+                {
+                    throw ClientUtility.CreateArgumentException("title");
+                }
+            }
+            object obj;
+            Dictionary<string, List> dictionary;
+            if (base.ObjectData.MethodReturnObjects.TryGetValue("GetByTitle", out obj))
+            {
+                dictionary = (Dictionary<string, List>)obj;
+            }
+            else
+            {
+                dictionary = new Dictionary<string, List>(StringComparer.OrdinalIgnoreCase);
+                base.ObjectData.MethodReturnObjects["GetByTitle"] = dictionary;
+            }
+            List list = null;
+            if (!context.DisableReturnValueCache && dictionary.TryGetValue(title, out list))
+            {
+                return list;
+            }
+            list = new List(context, new ObjectPathMethod(context, base.Path, "GetByTitle", new object[]
+            {
+                title
+            }));
+            if (!context.DisableReturnValueCache)
+            {
+                dictionary[title] = list;
+            }
+            ObjectIdentityQuery objectIdentityQuery = new ObjectIdentityQuery(list.Path);
+            context.AddQueryIdAndResultObject(objectIdentityQuery.Id, list);
+            context.AddQuery(objectIdentityQuery);
+            return list;
+        }
+
+        [Remote]
+        public List GetById(Guid id)
+        {
+            ClientRuntimeContext context = base.Context;
+            object obj;
+            Dictionary<Guid, List> dictionary;
+            if (base.ObjectData.MethodReturnObjects.TryGetValue("GetById", out obj))
+            {
+                dictionary = (Dictionary<Guid, List>)obj;
+            }
+            else
+            {
+                dictionary = new Dictionary<Guid, List>();
+                base.ObjectData.MethodReturnObjects["GetById"] = dictionary;
+            }
+            List list = null;
+            if (!context.DisableReturnValueCache && dictionary.TryGetValue(id, out list))
+            {
+                return list;
+            }
+            list = new List(context, new ObjectPathMethod(context, base.Path, "GetById", new object[]
+            {
+                id
+            }));
+            if (!context.DisableReturnValueCache)
+            {
+                dictionary[id] = list;
+            }
+            return list;
+        }
+
+        [Remote]
+        public List Add(ListCreationInformation parameters)
+        {
+            ClientRuntimeContext context = base.Context;
+            if (base.Context.ValidateOnClient)
+            {
+                if (parameters == null)
+                {
+                    throw ClientUtility.CreateArgumentNullException("parameters");
+                }
+                if (parameters != null)
+                {
+                    if (parameters.Title == null)
+                    {
+                        throw ClientUtility.CreateArgumentNullException("parameters.Title");
+                    }
+                    if (parameters.Title != null && parameters.Title.Length > 255)
+                    {
+                        throw ClientUtility.CreateArgumentException("parameters.Title");
+                    }
+                }
+            }
+            List list = new List(context, new ObjectPathMethod(context, base.Path, "Add", new object[]
+            {
+                parameters
+            }));
+            list.Path.SetPendingReplace();
+            ObjectIdentityQuery objectIdentityQuery = new ObjectIdentityQuery(list.Path);
+            context.AddQueryIdAndResultObject(objectIdentityQuery.Id, list);
+            context.AddQuery(objectIdentityQuery);
+            base.AddChild(list);
+            list.InitFromCreationInformation(parameters);
+            return list;
+        }
+
+        [Remote]
+        public List EnsureSitePagesLibrary()
+        {
+            ClientRuntimeContext context = base.Context;
+            List list = new List(context, new ObjectPathMethod(context, base.Path, "EnsureSitePagesLibrary", null));
+            list.Path.SetPendingReplace();
+            ObjectIdentityQuery objectIdentityQuery = new ObjectIdentityQuery(list.Path);
+            context.AddQueryIdAndResultObject(objectIdentityQuery.Id, list);
+            context.AddQuery(objectIdentityQuery);
+            return list;
+        }
+
+        [Remote]
+        public List EnsureSiteAssetsLibrary()
+        {
+            ClientRuntimeContext context = base.Context;
+            List list = new List(context, new ObjectPathMethod(context, base.Path, "EnsureSiteAssetsLibrary", null));
+            list.Path.SetPendingReplace();
+            ObjectIdentityQuery objectIdentityQuery = new ObjectIdentityQuery(list.Path);
+            context.AddQueryIdAndResultObject(objectIdentityQuery.Id, list);
+            context.AddQuery(objectIdentityQuery);
+            return list;
+        }
+    }
+}
---
+++ Microsoft.SharePoint.Client.NetCore/ListTemplateCollection.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.ListTemplateCollection", ServerTypeId = "{23748d10-16a1-4946-a38b-98fdec0e0ec8}")]
+    public class ListTemplateCollection : ClientObjectCollection<ListTemplate>
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ListTemplateCollection(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+
+        [Remote]
+        public ListTemplate GetByName(string name)
+        {
+            ClientRuntimeContext context = base.Context;
+            object obj;
+            Dictionary<string, ListTemplate> dictionary;
+            if (base.ObjectData.MethodReturnObjects.TryGetValue("GetByName", out obj))
+            {
+                dictionary = (Dictionary<string, ListTemplate>)obj;
+            }
+            else
+            {
+                dictionary = new Dictionary<string, ListTemplate>();
+                base.ObjectData.MethodReturnObjects["GetByName"] = dictionary;
+            }
+            ListTemplate listTemplate = null;
+            if (!context.DisableReturnValueCache && dictionary.TryGetValue(name, out listTemplate))
+            {
+                return listTemplate;
+            }
+            listTemplate = new ListTemplate(context, new ObjectPathMethod(context, base.Path, "GetByName", new object[]
+            {
+                name
+            }));
+            if (!context.DisableReturnValueCache)
+            {
+                dictionary[name] = listTemplate;
+            }
+            return listTemplate;
+        }
+    }
+}
