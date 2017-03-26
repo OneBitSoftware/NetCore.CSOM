2e83e963d3b71290c37fc772183846fa26662f56
Radi A <radi.a@live.com.au>
2017-03-26 19:22:47 +0300

    Form classes

---
+++ Microsoft.SharePoint.Client.NetCore/Form.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System;
+using System.ComponentModel;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.Form", ServerTypeId = "{50aaca3c-fa54-47d2-b946-a2839ee956a9}")]
+    public class Form : ClientObject
+    {
+        [Remote]
+        public Guid Id
+        {
+            get
+            {
+                base.CheckUninitializedProperty("Id");
+                return (Guid)base.ObjectData.Properties["Id"];
+            }
+        }
+
+        [Remote]
+        public ResourcePath ResourcePath
+        {
+            get
+            {
+                base.CheckUninitializedProperty("ResourcePath");
+                return (ResourcePath)base.ObjectData.Properties["ResourcePath"];
+            }
+        }
+
+        [Remote]
+        public string ServerRelativeUrl
+        {
+            get
+            {
+                base.CheckUninitializedProperty("ServerRelativeUrl");
+                return (string)base.ObjectData.Properties["ServerRelativeUrl"];
+            }
+        }
+
+        [Remote]
+        public PageType FormType
+        {
+            get
+            {
+                base.CheckUninitializedProperty("FormType");
+                return (PageType)base.ObjectData.Properties["FormType"];
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public Form(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+
+        protected override bool InitOnePropertyFromJson(string peekedName, JsonReader reader)
+        {
+            bool flag = base.InitOnePropertyFromJson(peekedName, reader);
+            if (flag)
+            {
+                return flag;
+            }
+            if (peekedName != null)
+            {
+                if (!(peekedName == "Id"))
+                {
+                    if (!(peekedName == "ResourcePath"))
+                    {
+                        if (!(peekedName == "ServerRelativeUrl"))
+                        {
+                            if (peekedName == "FormType")
+                            {
+                                flag = true;
+                                reader.ReadName();
+                                base.ObjectData.Properties["FormType"] = reader.ReadEnum<PageType>();
+                            }
+                        }
+                        else
+                        {
+                            flag = true;
+                            reader.ReadName();
+                            base.ObjectData.Properties["ServerRelativeUrl"] = reader.ReadString();
+                        }
+                    }
+                    else
+                    {
+                        flag = true;
+                        reader.ReadName();
+                        base.ObjectData.Properties["ResourcePath"] = reader.Read<ResourcePath>();
+                    }
+                }
+                else
+                {
+                    flag = true;
+                    reader.ReadName();
+                    base.ObjectData.Properties["Id"] = reader.ReadGuid();
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ Microsoft.SharePoint.Client.NetCore/FormCollection.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.SharePoint.Client.NetCore.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Microsoft.SharePoint.Client.NetCore
+{
+    [ScriptType("SP.FormCollection", ServerTypeId = "{078611ea-ce4d-45c0-9b7a-d4b1b46cc327}")]
+    public class FormCollection : ClientObjectCollection<Form>
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public FormCollection(ClientRuntimeContext context, ObjectPath objectPath) : base(context, objectPath)
+        {
+        }
+
+        [Remote]
+        public Form GetByPageType(PageType formType)
+        {
+            ClientRuntimeContext context = base.Context;
+            object obj;
+            Dictionary<PageType, Form> dictionary;
+            if (base.ObjectData.MethodReturnObjects.TryGetValue("GetByPageType", out obj))
+            {
+                dictionary = (Dictionary<PageType, Form>)obj;
+            }
+            else
+            {
+                dictionary = new Dictionary<PageType, Form>();
+                base.ObjectData.MethodReturnObjects["GetByPageType"] = dictionary;
+            }
+            Form form = null;
+            if (!context.DisableReturnValueCache && dictionary.TryGetValue(formType, out form))
+            {
+                return form;
+            }
+            form = new Form(context, new ObjectPathMethod(context, base.Path, "GetByPageType", new object[]
+            {
+                formType
+            }));
+            if (!context.DisableReturnValueCache)
+            {
+                dictionary[formType] = form;
+            }
+            return form;
+        }
+
+        [Remote]
+        public Form GetById(Guid id)
+        {
+            ClientRuntimeContext context = base.Context;
+            object obj;
+            Dictionary<Guid, Form> dictionary;
+            if (base.ObjectData.MethodReturnObjects.TryGetValue("GetById", out obj))
+            {
+                dictionary = (Dictionary<Guid, Form>)obj;
+            }
+            else
+            {
+                dictionary = new Dictionary<Guid, Form>();
+                base.ObjectData.MethodReturnObjects["GetById"] = dictionary;
+            }
+            Form form = null;
+            if (!context.DisableReturnValueCache && dictionary.TryGetValue(id, out form))
+            {
+                return form;
+            }
+            form = new Form(context, new ObjectPathMethod(context, base.Path, "GetById", new object[]
+            {
+                id
+            }));
+            if (!context.DisableReturnValueCache)
+            {
+                dictionary[id] = form;
+            }
+            return form;
+        }
+    }
+}
